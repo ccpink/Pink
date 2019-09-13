5d3a55764a7dae1133bfed53cd7de5228ab3d43c
ccpink <54963606+ccpink@users.noreply.github.com>
2019-09-13 12:02:03 -0300

    Add files via upload

---
+++ PROG1700_SourceCode/w0413534-PinkC/Sandbox/IfElse.py
@@ -0,0 +1,8 @@
+from random import *
+
+items = ['Alissa','Alice','Marco','Melissa','Sandra','Steve']
+print("Hello")
+x = sample(items,  1)   # Pick a random item from the list
+print(x[0])
+print("How is your day going")
+
