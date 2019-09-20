54fa05bd28389c374ecdda4dd16db3684e72f243
ccpink <54963606+ccpink@users.noreply.github.com>
2019-09-20 17:45:39 -0300

    Add files via upload

---
+++ PROG1700_SourceCode/w0413534-PinkC/Assignments/Assignment1/Assignment-1 Program 2 Loan Calculator.py
@@ -0,0 +1,22 @@
+"""
+Student Name:Charles Pink       
+Program Title:Loan Calculator
+Description:Calculates Loan payments    
+"""
+
+def main(): #<-- Don't change this line!
+    #Write your code below. It must be indented!
+
+#Inputs I need, amount of loan, number of years, interest rate
+
+#Calculations I need
+
+#Ouput
+
+
+
+    #Your code ends on the line above
+
+#Do not change any of the code below!
+if __name__ == "__main__":
+    main()
